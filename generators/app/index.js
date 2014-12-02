7026145b778b8591c295ea820adb609fa9bc2f7a
Rodolfo Torres <rtorres@simplifydigital.co.uk>
2014-12-02 09:17:41 +0000

    fixed issue with message

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,9 +3,10 @@ var generators = require('yeoman-generator');
 var fs = require('fs');
 
 var AppGenerator =  generators.Base.extend({
-  	constructor: function () {
-    this.log('based on generator-ko, please use it to generate your project')
-  },
+    message: function(){
+      console.log('based on generator-ko, please use it to generate your project');
+    }
+});
 	
 
 module.exports = AppGenerator;
