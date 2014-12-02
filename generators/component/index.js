71c20114c01184b17ecbdae7e5e07e8e17381785
Rodolfo Torres <rtorres@simplifydigital.co.uk>
2014-12-02 09:19:36 +0000

    formated

--- generators/component/index.js
+++ generators/component/index.js
@@ -4,12 +4,14 @@ var fs = require('fs');
 var chalk = require('chalk');
 
 var ComponentGenerator =  generators.Base.extend({
-  	constructor: function () {
+
+  constructor: function () {
     generators.Base.apply(this, arguments);
     this.argument('componentname', { type: String, required: true });
     this.componentname = this._.dasherize(this.componentname);
   },
-	prompting: function () {
+
+  prompting: function () {
     var done = this.async();
     this.prompt({
       type: 'input',
@@ -20,76 +22,79 @@ var ComponentGenerator =  generators.Base.extend({
     	this.category = answers.name;
     	
     	this.path = 'src/components/' + this.category + '/' + this.componentname + '/';
-      	done();
-    }.bind(this));
+     done();
+   }.bind(this));
   },
-  	init: function(){
+  
+  init: function(){
   	this.filename = 'sd-' + this.componentname;
   },
-  	copyREADME: function () {  		
-	    this.fs.copyTpl(
-	      this.templatePath('README.md'),
-	      this.destinationPath(this.path + 'README.md'),
-	      { componentname: this.componentname }
-	    );
-  },
-  	createViewModel: function () {
-  		
-	    this.fs.copyTpl(
-	      this.templatePath('viewModel.js'),
-	      this.destinationPath(this.path + this.filename +'.js'),
-	      { viewModelClassName: this._.classify(this.componentname + 'ViewModel'),
-	      	filename: this.filename,
-	      	name: this.componentname
-	       }
-	    );
-  },
 
-	createView: function () {
-	    this.fs.copyTpl(
-	      this.templatePath('view.html'),
-	      this.destinationPath(this.path + this.filename +'.html'),
-	      { name: this.componentname }
-	    );
-  },
+  copyREADME: function () {  		
+   this.fs.copyTpl(
+     this.templatePath('README.md'),
+     this.destinationPath(this.path + 'README.md'),
+     { componentname: this.componentname }
+     );
+ },
+ 
+ createViewModel: function () {
 
-  	createTest: function () {
-	    this.fs.copyTpl(
-	      this.templatePath('test.js'),
-	      this.destinationPath(this.path + this.filename +'-test.js'),
-	      { name: this.componentname }
-	    );
-  },
+   this.fs.copyTpl(
+     this.templatePath('viewModel.js'),
+     this.destinationPath(this.path + this.filename +'.js'),
+     { viewModelClassName: this._.classify(this.componentname + 'ViewModel'),
+     filename: this.filename,
+     name: this.componentname
+   }
+   );
+ },
 
-  addComponentRegistration: function() {
-    var startupFile = 'src/app/startup.js';
-    readIfFileExists.call(this, startupFile, function(existingContents) {
-        var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.filename + '[\'"]');
-        if (existingRegistrationRegex.exec(existingContents)) {
-            this.log(this.filename + ' is already registered in ' + startupFile);
-            return;
-        }
+ createView: function () {
+   this.fs.copyTpl(
+     this.templatePath('view.html'),
+     this.destinationPath(this.path + this.filename +'.html'),
+     { name: this.componentname }
+     );
+ },
 
-        var token = '// [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]',
-            regex = new RegExp('^(\\s*)(' + token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') + ')', 'm'),
-            modulePath = 'components/' + this.category + '/' + this.componentname + '/' + this.filename,
-            lineToAdd = 'ko.components.register(\'' + this.filename + '\', { require: \'' + modulePath + '\' });',
-            newContents = existingContents.replace(regex, '$1' + lineToAdd + '\n$&');
-        fs.writeFile(startupFile, newContents);
-        this.log('   Registered ' + this.filename + ' in '+ startupFile);
+ createTest: function () {
+   this.fs.copyTpl(
+     this.templatePath('test.js'),
+     this.destinationPath(this.path + this.filename +'-test.js'),
+     { name: this.componentname }
+     );
+ },
 
-        if (fs.existsSync('gulpfile.js')) {
-            this.log(chalk.magenta('To include in build output, reference ') + chalk.white('\'' + modulePath + '\'') + chalk.magenta(' in ') + chalk.white('gulpfile.js'));
-        }
+ addComponentRegistration: function() {
+  var startupFile = 'src/app/startup.js';
+  readIfFileExists.call(this, startupFile, function(existingContents) {
+    var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.filename + '[\'"]');
+      if (existingRegistrationRegex.exec(existingContents)) {
+        this.log(this.filename + ' is already registered in ' + startupFile);
+        return;
+      }
+
+      var token = '// [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]',
+      regex = new RegExp('^(\\s*)(' + token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') + ')', 'm'),
+      modulePath = 'components/' + this.category + '/' + this.componentname + '/' + this.filename,
+      lineToAdd = 'ko.components.register(\'' + this.filename + '\', { require: \'' + modulePath + '\' });',
+      newContents = existingContents.replace(regex, '$1' + lineToAdd + '\n$&');
+      fs.writeFile(startupFile, newContents);
+      this.log('   Registered ' + this.filename + ' in '+ startupFile);
+
+      if (fs.existsSync('gulpfile.js')) {
+        this.log(chalk.magenta('To include in build output, reference ') + chalk.white('\'' + modulePath + '\'') + chalk.magenta(' in ') + chalk.white('gulpfile.js'));
+      }
     });
-  }
+}
 
 });
 
 function readIfFileExists(path, callback) {
-    if (fs.existsSync(path)) {
-        callback.call(this, this.readFileAsString(path));
-    }
+  if (fs.existsSync(path)) {
+    callback.call(this, this.readFileAsString(path));
+  }
 }
 
 module.exports = ComponentGenerator;
